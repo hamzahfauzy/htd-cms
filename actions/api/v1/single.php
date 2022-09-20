b091d4fe8472671f2bbe6c88e0dda3e6defaff73
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-09-20 21:37:52 +0700

    update single api

--- actions/api/v1/single.php
+++ actions/api/v1/single.php
@@ -8,6 +8,15 @@ $data = $db->all('posts',[
     'status' => 'Publish'
 ]);
 
+$data = array_map(function($p) use ($db){
+    $category_posts = $db->all('category_posts',['post_id'=>$p->id]);
+    $cat_ids = array_map(function($cat){
+        return $cat->category_id;
+    }, $category_posts);
+    $p->categories = $db->all('categories',['id'=>['IN','('.implode(',',$cat_ids).')']]);
+    return $p;
+}, $data);
+
 echo json_encode([
     'status'  => 'success',
     'message' => 'data retrieved',
